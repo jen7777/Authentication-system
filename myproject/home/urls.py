16ba96d7c73dfa46219ecb431b3eeb33ee6869bb
jen7777 <66324489+jen7777@users.noreply.github.com>
2023-12-05 23:35:59 -0600

    Frontend ready

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -6,4 +6,6 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('',views.index,name='home'),
+    path('signup/',views.signup,name='signup'),
+    path('signin/',views.signin,name='signin'),
 ]
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
+def signup(request):
+    return render(request,'signup.html')
+def signin(request):
+    return render(request,'signin.html')
