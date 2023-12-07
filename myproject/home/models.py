489bc7a35974f3504c84cd6d45f69e860b2fc880
jen7777 <66324489+jen7777@users.noreply.github.com>
2023-12-07 14:51:45 -0600

    Input adding to database

--- myproject/home/models.py
+++ myproject/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class signup(models.Model):
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    email=models.EmailField()
+    password1=models.CharField(max_length=255)
+    password2=models.CharField(max_length=255)
+    
+    
--- myproject/home/views.py
+++ myproject/home/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import signup
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+
 def index(request):
     return render(request,'index.html')
 def signup(request):
+    if request.method == 'POST':
+
+        username=request.POST.get('username')                           #adding recieved name to new variable username
+        firstname=request.POST.get('firstname') 
+        lastname=request.POST.get('lastname')                    
+        password1=request.POST.get('password1')
+        
+        myuser=User.objects.create_user(username,email,password1)       #creating new object myuser for inbuilt model class Users
+        myuser.first_name = firstname                                   #adding variables using object myuser 
+        myuser.last_name = lastname
+        
+        myuser.save()    
     return render(request,'signup.html')
 def signin(request):
     return render(request,'signin.html')
